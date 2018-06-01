973c43965d4a7a5724c28d0ee0bcf48b31f3e493
Sheryl John <sheryljj@gmail.com>
2018-06-01 16:03:50 -0700

    add login page

--- webpack.config.js
+++ webpack.config.js
@@ -17,23 +17,9 @@ const config = {
 				use: 'babel-loader'
 			},
 			{
-				test: /\.css$/,
-		        use: [
-		          {
-		            loader: "style-loader"
-		          },
-		          {
-		            loader: "css-loader",
-		            options: {
-		              modules: true,
-		              importLoaders: 1,
-		              localIdentName: "[name]_[local]_[hash:base64]",
-		              sourceMap: true,
-		              minimize: true
-		            }
-		          }
-		        ]
-			}
+		        test: /\.css$/,
+		        use: ['style-loader', 'css-loader']
+		    }
 		]
 	},
 	plugins: [htmlWebpackPlugin]
