6b6078e4ffc74c472077959f5754f4ea75970baa
Sheryl John <sheryljj@gmail.com>
2018-06-05 11:01:24 -0700

    redux action and reducers with thunk for login session

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ import {
 } from 'reactstrap';
 
 import styles from './app.css';
+import { connect } from 'react-redux';
+import { createSession } from './App/loginActions';
 
 class App extends Component {
     constructor(props) {
@@ -30,24 +32,31 @@ class App extends Component {
             isOpen: !this.state.isOpen
         });
     }
+    onSubmit() {
+        console.log('test');
+        this.props.dispatch(createSession('sherylj', '3p4dt3st'));
+    }
     render() {
         return (
-            <div className="bg-gradient-dark">
-                <Navbar color="dark" dark expand="md">
-                    <NavbarBrand href="/">ePAD</NavbarBrand>
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-                    	<div className="bg-dark">
-                        <Row>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a className="navbar-brand" href="#">ePAD</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarColor01">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+                        <Row className={styles.topBuffer}>
                         	<Col sm="4"/>
                             <Col sm="4">
-                            	<Card body dark="true">
+                            	<Card body>
                                 <form className={styles.formSignin}>
 							      <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
 							      <label htmlFor="inputEmail" className="sr-only">Email address</label>
@@ -56,19 +65,26 @@ class App extends Component {
 							      <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
 							      <div className="checkbox mb-3">
 							        <label>
-							          <input type="checkbox" value="remember-me" /> I agree to <a href="#">Terms and Conditions</a>
+							          <input type="checkbox" value="remember-me" /> I agree to <a color="primary" href="#">Terms and Conditions</a>
 							        </label>
 							      </div>
-							      <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+							      <button className="btn btn-primary" type="submit" onClick={this.onSubmit.bind(this)}>Sign in</button>
+                                  <p />
                                   <a href="#">Forgot Password?</a>
 							    </form>
 							    </Card>
                             </Col>
                         </Row>
-                        </div>
             </div>
         );
     }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  token: state.token,
+  loading: state.loading,
+  error: state.login.error
+});
+
+
+export default connect(mapStateToProps)(App);
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import login from '../App/loginReducer';
+
+export default combineReducers({
+	login
+});
